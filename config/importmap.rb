2d398e29ac7f297b10d91b937e53ab5da464d98f
hikaru141592 <hikaruit0030@gmail.com>
2025-05-15 01:41:46 +0900

    bin/importmap jsonが正しく出力されるように再設定

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
 pin "application"
-pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "bootstrap",  to: "bootstrap.bundle.min.js"
 
-pin "@rails/ujs",                to: "@rails--ujs.js"
-pin "@hotwired/turbo-rails",     to: "turbo.min.js",        preload: true
-pin "@hotwired/stimulus",        to: "stimulus.umd.js",     preload: true
-pin "@hotwired/stimulus-loading",to: "stimulus-loading.js"
+pin "rails-ujs",                to: "rails-ujs.js"
+pin "@rails/ujs",               to: "rails-ujs.js"
+pin "@hotwired/turbo-rails",    to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus",       to: "stimulus.js",   preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
