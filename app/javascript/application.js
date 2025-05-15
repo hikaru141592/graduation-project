a5d145db14b5e8d42014bf7b1438ad279764285d
hikaru141592 <hikaruit0030@gmail.com>
2025-05-16 00:38:20 +0900

    rails-ujs.jsやstimulus-loading.jsのSyntaxError対処

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo"
+import "@rails/ujs"
+import "bootstrap"
+import "@hotwired/stimulus"
+import "@hotwired/stimulus-loading"
