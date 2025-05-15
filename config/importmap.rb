b4a7a6d50bc108c23a9a1e6a2d64ac7ae983f16f
hikaru141592 <hikaruit0030@gmail.com>
2025-05-15 20:40:03 +0900

    Tuboが使えるようになりログアウトが可能に

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,6 @@ pin "bootstrap",  to: "bootstrap.bundle.min.js"
 
 pin "rails-ujs",                to: "rails-ujs.js"
 pin "@rails/ujs",               to: "rails-ujs.js"
-pin "@hotwired/turbo-rails",    to: "turbo.min.js", preload: true
+pin "@hotwired/turbo",          to: "turbo.es2017-esm.js", preload: true
 pin "@hotwired/stimulus",       to: "stimulus.js",   preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
