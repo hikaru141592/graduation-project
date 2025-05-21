e4b630397e95b0982e7a554e18c5a18248283dc1
hikaru141592 <hikaruit0030@gmail.com>
2025-05-22 02:24:22 +0900

    Rubocopエラー修正

--- Gemfile
+++ Gemfile
@@ -71,5 +71,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   # gem 'webdrivers'
-  gem 'database_cleaner-active_record'
+  gem "database_cleaner-active_record"
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,6 +50,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
-  #config.logger = ActiveSupport::Logger.new($stdout)
-  #config.log_level = :debug
+  # config.logger = ActiveSupport::Logger.new($stdout)
+  # config.log_level = :debug
 end
