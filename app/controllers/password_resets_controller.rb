387e08794a65075c6b849cf0779f93f3efba382a
hikaru141592 <hikaruit0030@gmail.com>
2025-06-10 17:04:03 +0900

    Rubocop修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
       user.deliver_reset_password_instructions!
       UserMailer.reset_password_email(user).deliver_now
     end
-    redirect_to password_resets_create_path, success: t('password_resets.create.success', default: 'パスワードリセット手順を送信しました')
+    redirect_to password_resets_create_path, success: t("password_resets.create.success", default: "パスワードリセット手順を送信しました")
   end
 
   def create_page
@@ -20,7 +20,7 @@ class PasswordResetsController < ApplicationController
     @token = params[:token].to_s
     @user = User.load_from_reset_password_token(@token)
     if @user.nil?
-      flash[:alert] = t('password_resets.edit.invalid_token', default: 'リンクが無効か、有効期限が切れています。再度お試しください。')
+      flash[:alert] = t("password_resets.edit.invalid_token", default: "リンクが無効か、有効期限が切れています。再度お試しください。")
       redirect_to new_password_reset_path
     end
   end
@@ -29,22 +29,22 @@ class PasswordResetsController < ApplicationController
     @token = params[:token].to_s
     @user = User.load_from_reset_password_token(@token)
     if @user.nil?
-      flash[:alert] = t('password_resets.edit.invalid_token', default: 'リンクが無効か、有効期限が切れています。再度お試しください。')
+      flash[:alert] = t("password_resets.edit.invalid_token", default: "リンクが無効か、有効期限が切れています。再度お試しください。")
       redirect_to new_password_reset_path and return
     end
 
     password = params[:user][:password].to_s
     password_confirmation = params[:user][:password_confirmation].to_s
     if password != password_confirmation
-      flash.now[:alert] = t('password_resets.update.password_mismatch', default: 'パスワードと確認用パスワードが一致しません。')
+      flash.now[:alert] = t("password_resets.update.password_mismatch", default: "パスワードと確認用パスワードが一致しません。")
       render :edit, status: :unprocessable_entity and return
     end
 
     @user.password_confirmation = password_confirmation
     if @user.change_password!(password)
-      redirect_to complete_update_password_reset_path, success: t('password_resets.update.success', default: 'パスワードを変更しました。')
+      redirect_to complete_update_password_reset_path, success: t("password_resets.update.success", default: "パスワードを変更しました。")
     else
-      flash.now[:alert] = @user.errors.full_messages.join(', ')
+      flash.now[:alert] = @user.errors.full_messages.join(", ")
       render :edit, status: :unprocessable_entity
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
 
   get "/privacy_policy", to: "privacy_policies#show", as: :privacy_policy
 
-  get  '/password_resets/new',     to: 'password_resets#new',    as: 'new_password_reset'
-  post '/password_resets',         to: 'password_resets#create', as: 'password_resets'
-  get   '/password_resets/create',   to: 'password_resets#create_page', as: 'password_resets_create'
-  get   '/password_resets/edit',   to: 'password_resets#edit',   as: 'edit_password_reset'
-  patch '/password_resets/update', to: 'password_resets#update', as: 'update_password_reset'
-  get '/password_resets/complete_update', to: 'password_resets#complete_update', as: 'complete_update_password_reset'
+  get  "/password_resets/new",     to: "password_resets#new",    as: "new_password_reset"
+  post "/password_resets",         to: "password_resets#create", as: "password_resets"
+  get   "/password_resets/create",   to: "password_resets#create_page", as: "password_resets_create"
+  get   "/password_resets/edit",   to: "password_resets#edit",   as: "edit_password_reset"
+  patch "/password_resets/update", to: "password_resets#update", as: "update_password_reset"
+  get "/password_resets/complete_update", to: "password_resets#complete_update", as: "complete_update_password_reset"
 
   root to: "games#play"
 
