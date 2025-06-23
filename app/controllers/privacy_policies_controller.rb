aef6fd35e6130c25ad9ac074a71dd0b355111ad3
hikaru141592 <hikaruit0030@gmail.com>
2025-06-23 19:58:07 +0900

    ユーザー情報補完画面レイアウト調整

--- app/controllers/privacy_policies_controller.rb
+++ app/controllers/privacy_policies_controller.rb
@@ -1,5 +1,6 @@
 class PrivacyPoliciesController < ApplicationController
   skip_before_action :require_login, only: %i[show]
+  skip_before_action :redirect_if_incomplete_profile, only: %i[show]
 
   def show
   end
