de58a6c752a33c3db4b4c4913c41b78a35e6677a
hikaru141592 <hikaruit0030@gmail.com>
2025-07-03 16:40:42 +0900

    派生イベント実装、派生イベントとして「べんきょうする」イベントデータをseedで作成

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -93,18 +93,23 @@ class GamesController < ApplicationController
       resolves = result.resolves_loop?
 
       current_user.user_event_category_invalidations.where("expires_at < ?", Time.current).delete_all
-      if result.resolves_loop?
-        event_category_invalidation(current_user, current_set.event_category, 2.hours.from_now)
-      end
-      if continue_loop?(user_status, current_set, resolves)
-        next_set = current_set
-        next_event = event
+
+      if result.next_derivation_number.present?
+        next_set, next_event = apply_derivation(result)
       else
-        clear_loop_status(user_status)
-        next_set, next_event = pick_next_event_set_and_event
-        next_set, next_event = apply_event_set_call(result, next_set, next_event)
-        next_set, next_event = apply_event_set_call(result, next_set, next_event)
-        record_loop_start(current_user.user_status, next_set)
+        if resolves
+          event_category_invalidation(current_user, current_set.event_category, 2.hours.from_now)
+        end
+        if continue_loop?(user_status, current_set, resolves)
+          next_set = current_set
+          next_event = event
+        else
+          clear_loop_status(user_status)
+          next_set, next_event = pick_next_event_set_and_event
+          next_set, next_event = apply_event_set_call(result, next_set, next_event)
+          next_set, next_event = apply_event_set_call(result, next_set, next_event)
+          record_loop_start(current_user.user_status, next_set)
+        end
       end
 
       play_state.update!(
@@ -229,5 +234,17 @@ class GamesController < ApplicationController
     else
       [ default_set, default_event ]
     end
+  end
+
+  def apply_derivation(result)
+    derivation_num = result.next_derivation_number
+    event_set = result.action_choice.event.event_set
+    begin
+      derived_event = event_set.events.find_by!(derivation_number: derivation_num)
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.warn "Derivation event not found: set=#{event_set.id}, num=#{derivation_num}"
+      derived_event = event_set.events.find_by!(derivation_number: 0)
+    end
+    [ event_set, derived_event ]
   end
 end
