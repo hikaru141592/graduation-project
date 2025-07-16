4fdadfff8e6b1f1000b2adddcbadb9b882ea5b67
hikaru141592 <hikaruit0030@gmail.com>
2025-07-16 20:27:20 +0900

    LINEメッセージ送信ジョブ（テスト用）作成

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "cssbundling-rails"
 gem "omniauth-line"
 gem "omniauth-rails_csrf_protection"
 gem "meta-tags"
+gem 'line-bot-api'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ app/jobs/seasonal_notification_job.rb
@@ -0,0 +1,38 @@
+require 'line/bot'
+
+class SeasonalNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    today = Date.current
+
+    start_date = Date.new(today.year, 7, 1)
+    end_date   = Date.new(today.year, 9, 15)
+
+    return unless (start_date..end_date).cover?(today)
+
+    message = {
+      type: 'text',
+      text: "もう夏だね！暑いときは扇風機の風にでもあたって気持ちよくなりたいなあ！熱中症には気を付けてね！"
+    }
+
+    User.where(line_notifications_enabled: true)
+        .joins(:authentications)
+        .where(authentications: { provider: 'line' })
+        .pluck('authentications.uid')
+        .each do |uid|
+      line_client.push_message(uid, message)
+    end
+
+    Rails.logger.info "[SeasonalNotificationJob] #{today}の通知を完了しました。"
+  end
+
+  private
+
+  def line_client
+    @line_client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token  = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+end
