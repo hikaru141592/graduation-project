bf35014630e736f177112e350ad69c7825722633
hikaru141592 <hikaruit0030@gmail.com>
2025-07-30 21:23:00 +0900

    モーダルでステータス表示を可能に

--- app/javascript/controllers/status_controller.js
+++ app/javascript/controllers/status_controller.js
@@ -1,6 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
-import { Moda}        from "bootstrap";
+import "bootstrap" 
 
 export default class extends Controller {
-  
+  static targets = ["modal", "frame"];
+
+  connect() {
+    this.modal = bootstrap.Modal.getOrCreateInstance(this.modalTarget)
+  }
+  open() {
+    console.log("▶️ status#open fired")
+    fetch("/status")
+      .then(r => r.text())
+      .then(html => {
+        this.frameTarget.innerHTML = html;
+        this.modal.show();
+      });
+  }
 }
