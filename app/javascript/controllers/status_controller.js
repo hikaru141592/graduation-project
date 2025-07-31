95aaaceb7e438334811c4fb83a578ba24979f7a9
hikaru141592 <hikaruit0030@gmail.com>
2025-07-31 20:20:23 +0900

    閉じたモーダルが勝手に再表示されるバグ修正

--- app/javascript/controllers/status_controller.js
+++ app/javascript/controllers/status_controller.js
@@ -5,13 +5,31 @@ export default class extends Controller {
   static targets = ["modal", "frame"];
 
   connect() {
+    console.log("connect() called");
+    console.trace();
     this.modal = bootstrap.Modal.getOrCreateInstance(this.modalTarget)
+
+    this.isClosed = false;
+    this.modalTarget.addEventListener(
+      "hidden.bs.modal",
+      () => { this.isClosed = true; }
+    );
   }
   open() {
-    console.log("▶️ status#open fired")
+    this.isClosed = false;
+
+    console.log("open() START at", performance.now());
     fetch("/status")
       .then(r => r.text())
       .then(html => {
+        console.log("fetch resolved at", performance.now());
+        console.trace();
+
+        if (this.isClosed) {
+          console.log("fetch finished but modal already closed – skip show()");
+          return;
+        }
+
         this.frameTarget.innerHTML = html;
         this.modal.show();
       });
