a8456604c52651c1d613a93d137e30ec9ab66d7a
hikaru141592 <hikaruit0030@gmail.com>
2025-08-04 20:27:55 +0900

    Rubocop修正

--- app/helpers/message_builder.rb
+++ app/helpers/message_builder.rb
@@ -5,7 +5,7 @@ class MessageBuilder
     @question_text = question_text
     @current_event = play_state.current_event
   end
-  
+
   def call
     message = @play_state.current_cut&.random_message(@play_state.updated_at) || @current_event.message
     message = replace_question_text(message)
@@ -18,25 +18,25 @@ class MessageBuilder
 
   def replace_question_text(message)
     return message unless @question_text.present?
-    message.gsub('X 演算子 Y', @question_text)
+    message.gsub("X 演算子 Y", @question_text)
   end
 
   def replace_egg_name(message)
-    message.gsub('〈たまご〉', @user.egg_name_with_suffix)
+    message.gsub("〈たまご〉", @user.egg_name_with_suffix)
   end
 
   def replace_user_name(message)
-    message.gsub('〈ユーザー〉', @user.name)
+    message.gsub("〈ユーザー〉", @user.name)
   end
 
   def replace_training_report(message)
-    return message unless @current_event.event_set.name == '特訓' && (2..6).include?(@current_event.derivation_number)
+    return message unless @current_event.event_set.name == "特訓" && (2..6).include?(@current_event.derivation_number)
     temp = @user.event_temporary_datum
     success_count = temp.success_count
-    message = message.gsub('〈X〉', success_count.to_s)
+    message = message.gsub("〈X〉", success_count.to_s)
     elapsed_sec = (temp.ended_at - temp.started_at).to_i
     minutes, seconds = elapsed_sec.divmod(60)
     time_str    = "#{minutes}ふん#{seconds}びょう"
-    message     = message.gsub('〈Y〉分〈Z〉秒', time_str)
+    message     = message.gsub("〈Y〉分〈Z〉秒", time_str)
   end
 end
