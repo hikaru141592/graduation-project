de6519961af0254c3a111becfb4c074a541165e0
hikaru141592 <hikaruit0030@gmail.com>
2025-08-22 20:31:02 +0900

    importmap.rbから不要な行削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,8 @@
 pin "application"
 pin "bootstrap",  to: "bootstrap.bundle.min.js"
 
-pin "rails-ujs",                to: "rails-ujs.js"
 pin "@rails/ujs",               to: "rails-ujs.js"
 pin "@hotwired/turbo",          to: "turbo.es2017-esm.js", preload: true
-pin "@hotwired/stimulus",       to: "stimulus.js",   preload: true
+pin "@hotwired/stimulus",       to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
