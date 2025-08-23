1e7986660446492a40a546f91ce9dd211f54cd08
hikaru141592 <hikaruit0030@gmail.com>
2025-08-23 21:55:29 +0900

    ABOUTページ作成

--- spec/requests/public_pages_spec.rb
+++ spec/requests/public_pages_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "PublicPages", type: :request do
   describe "GET /about" do
-    xit "returns http success" do
+    it "returns http success" do
       get about_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /terms" do
+  describe "GET /term" do
     it "returns http success" do
       get term_path
       expect(response).to have_http_status(:success)
