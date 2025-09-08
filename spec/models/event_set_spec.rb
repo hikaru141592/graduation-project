cc86d6495dc5e8bf5a40cac5bba4188be3a06b4d
hikaru141592 <hikaruit0030@gmail.com>
2025-09-08 16:58:49 +0900

    event_set_spec作成

--- spec/models/event_set_spec.rb
+++ spec/models/event_set_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EventSet, type: :model do
-  let(:category) { build(:event_category) }
+  let(:category) { create(:event_category) }
   let(:valid_attrs) do
     {
       event_category:     category,
@@ -32,5 +32,26 @@ RSpec.describe EventSet, type: :model do
     dup = build(:event_set, valid_attrs)
     expect(dup).not_to be_valid
     expect(dup.errors.details[:name]).to include(a_hash_including(error: :taken))
+  end
+
+  it 'trigger_conditionsが空だと無効になる' do
+    set = build(:event_set, valid_attrs.merge(trigger_conditions: nil))
+    expect(set).not_to be_valid
+    expect(set.errors.details[:trigger_conditions]).to include(a_hash_including(error: :blank))
+  end
+
+  it 'event_setを削除すると関連するeventsも削除される' do
+    set = create(:event_set, event_category: category)
+    event = create(:event, event_set: set)
+    set.destroy
+    expect(Event.where(id: event.id)).to be_empty
+  end
+
+  it 'event_setを削除すると関連するdaily_limit_event_set_countsも削除される' do
+    set = create(:event_set, event_category: category)
+    user = create(:user)
+    count = DailyLimitEventSetCount.create!(user: user, event_set: set, occurred_on: Date.current, count: 1)
+    set.destroy
+    expect(DailyLimitEventSetCount.where(id: count.id)).to be_empty
   end
 end
