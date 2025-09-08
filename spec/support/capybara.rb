c841c87e6b1e81014d561a0dfb6428495f8cc081
hikaru141592 <hikaruit0030@gmail.com>
2025-09-08 14:46:30 +0900

    不要なコメント行削除

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,18 +1,16 @@
-# spec/support/capybara.rb
 require 'capybara/rails'
 require 'selenium/webdriver'
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  # 必要最小限の引数だけ
   %w[headless disable-gpu no-sandbox disable-dev-shm-usage].each do |flag|
     options.add_argument("--#{flag}")
   end
 
-  # ドライバ自動検出に任せる（chromedriver・chromeのパスを個別指定しない）
+  # SeleniumとChromeは自動で探す
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-# JS テスト時のドライバとして使う
+# JSテスト時のドライバ用
 Capybara.javascript_driver = :headless_chrome
 Capybara.default_max_wait_time = 5
