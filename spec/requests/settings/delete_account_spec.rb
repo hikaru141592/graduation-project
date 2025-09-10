cd7e13f33b0734fe3ab504296c4a31e6aae3375b
hikaru141592 <hikaruit0030@gmail.com>
2025-09-10 18:53:22 +0900

    requests/settings/delete_account_spec.rb修正

--- spec/requests/settings/delete_account_spec.rb
+++ spec/requests/settings/delete_account_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Settings::Accounts", type: :request do
+RSpec.describe "アカウント削除", type: :request do
   let(:user) { create(:user, password: "password", password_confirmation: "password") }
 
   before do
@@ -8,20 +8,23 @@ RSpec.describe "Settings::Accounts", type: :request do
   end
 
   describe "GET /settings/delete_account" do
-    it "returns http success" do
+    it "削除ページが表示される" do
       get settings_delete_account_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include(I18n.t("settings.accounts.show.title"))
+      expect(response.body).to include(I18n.t("buttons.user_destroy"))
     end
   end
 
   describe "DELETE /settings/delete_account" do
-    it "deletes the account and redirects" do
+    it "アカウント削除後、ログイン画面にリダイレクトされる" do
       expect {
         delete settings_delete_account_path
       }.to change(User, :count).by(-1)
-
       expect(response).to have_http_status(:see_other)
       expect(response).to redirect_to(new_session_path)
+      follow_redirect!
+      expect(response.body).to include(I18n.t("flash.settings.accounts.destroy.danger"))
     end
   end
 end
