775f48bdff153f02b58a36627546c8ce879a75b4
hikaru141592 <hikaruit0030@gmail.com>
2025-09-16 21:36:19 +0900

    ActionChoiceモデルのconditions_met?メソッドをConditionEvaluatorサービスクラスのconditions_met?メソッドに置き換え

--- app/models/action_choice.rb
+++ app/models/action_choice.rb
@@ -6,29 +6,8 @@ class ActionChoice < ApplicationRecord
   validates :label,    presence: true
 
   def selected_result(user)
-    result   = action_results.order(:priority).detect { |ar| conditions_met?(ar.trigger_conditions, user) }
-    result ||= action_results.order(:priority).first
-  end
-
-  private
-  def conditions_met?(conds, user)
-    return true if conds["always"] == true
-    op   = conds["operator"] || "and"
-    list = conds["conditions"] || []
-    results = list.map do |c|
-      case c["type"]
-      when "status"
-        user.user_status.public_send(c["attribute"]).public_send(c["operator"], c["value"])
-      when "probability"
-        rand(100) < c["percent"]
-      when "item"
-        user.user_items.find_by(code: c["item_code"]).try(:count).to_i.public_send(c["operator"], c["value"])
-      when "event_temporary_data"
-        user.event_temporary_datum.public_send(c["attribute"]).public_send(c["operator"], c["value"])
-      else
-        false
-      end
-    end
-    op == "and" ? results.all? : results.any?
+    ordered  = action_results.order(:priority).to_a
+    result   = ordered.detect { |ar| ConditionEvaluator.new(user, ar.trigger_conditions).conditions_met? }
+    result ||= ordered.first
   end
 end
